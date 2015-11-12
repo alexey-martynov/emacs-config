7c5e9788fda603faf8e49f92744ac267cdf3b9da
Alexey Martynov <alexey_martynov@rbcmail.ru>
2015-11-12 15:47:17 +0300

    Set correct font for LaTeX verbatim environments

--- init.el
+++ init.el
@@ -106,6 +106,13 @@
  '(visible-bell t)
  '(which-function-mode t))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tex-verbatim ((t (:inherit nil)))))
+
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq make-backup-files nil)
 (setq split-width-threshold most-positive-fixnum)
