dddd7c2cd4add6e9df911cd8f8f5381695945cf9
Alexey Martynov <alexey_martynov@rbcmail.ru>
2016-09-20 12:43:13 +0300

    Make javascript indentation variable buffer local

--- modes/javascript.el
+++ modes/javascript.el
@@ -1,6 +1,7 @@
 (defun avm-js-mode-hook ()
   (define-key js-mode-map (kbd "<RET>") 'newline-and-indent)
   (define-key js-mode-map (kbd "C-c o") 'ff-find-other-file)
+  (make-local-variable 'js-indent-level)
   (setq js-indent-level 2)
   (setq show-trailing-whitespace t)
   (electric-indent-mode t)
