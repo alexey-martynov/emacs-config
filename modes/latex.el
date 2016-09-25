4e782d10a359e2ed7f8d47a4d25c740ae53adc23
Alexey Martynov <martynov.alexey@gmail.com>
2016-09-25 13:15:00 +0300

    Cleanup LaTeX mode config

--- modes/latex.el
+++ modes/latex.el
@@ -19,20 +19,27 @@
      (insert "\\label{}")
      (backward-char))))
 
-(add-hook 'latex-mode-hook
-          '(lambda ()
-            (define-key latex-mode-map (kbd "<RET>") 'newline-and-indent)
-            (define-key latex-mode-map (kbd "C-c r") 'latex-insert-reference)
-            (define-key latex-mode-map (kbd "C-c l") 'latex-insert-label)
-            (turn-on-auto-fill)
-            (setq show-trailing-whitespace t)
-            (delete-trailing-whitespace-mode 'clean)
-            (local-set-key (kbd "<f7>") 'compile)
-            ;; Turn on Russian typesetting features
-            (setq-default tex-open-quote "<<")
-            (setq-default tex-close-quote ">>")
-            (add-to-list 'tex-verbatim-environments "cxxsource")
-            (add-to-list 'tex-verbatim-environments "lispsource")
-            (add-to-list 'tex-verbatim-environments "lisprepl")
-            (add-to-list 'tex-verbatim-environments "console")
-            ))
+(defun avm-latex-mode-hook ()
+  (define-key latex-mode-map (kbd "<RET>") 'newline-and-indent)
+  (define-key latex-mode-map (kbd "C-c r") 'latex-insert-reference)
+  (define-key latex-mode-map (kbd "C-c l") 'latex-insert-label)
+  (turn-on-auto-fill)
+  (setq show-trailing-whitespace t)
+  (delete-trailing-whitespace-mode 'clean)
+  (local-set-key (kbd "<f7>") 'compile)
+
+  ;; Always turn on Flyspell
+  (flyspell-mode t)
+
+  ;; Turn on Russian typesetting features
+  (setq-default tex-open-quote "<<")
+  (setq-default tex-close-quote ">>")
+
+  ;; Extend list of verbatim environments with
+  ;; handcrafted envirinments for seminars
+  (add-to-list 'tex-verbatim-environments "cxxsource")
+  (add-to-list 'tex-verbatim-environments "lispsource")
+  (add-to-list 'tex-verbatim-environments "lisprepl")
+  (add-to-list 'tex-verbatim-environments "console"))
+
+(add-hook 'latex-mode-hook #'avm-latex-mode-hook)
