e00b700be4519e433477caf08ea67758fe4c4919
Alexey Martynov <martynov.alexey@gmail.com>
2024-03-18 19:53:09 +0300

    Do not save authentication information

--- init.el
+++ init.el
@@ -47,10 +47,11 @@
   (load-theme 'twilight t);)
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(compilation-read-command nil)
