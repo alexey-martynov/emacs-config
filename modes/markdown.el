8e4d19fccafd91ee0ee2fa61aad7294bd6cfb82a
Alexey Martynov <martynov.alexey@gmail.com>
2024-04-05 22:05:16 +0300

    Turn on auto-fill and flyspell in Markdown modes

--- modes/markdown.el
+++ modes/markdown.el
@@ -1,13 +1,21 @@
+(defun avm-markdown-mode-hook ()
+  (delete-trailing-whitespace-mode 'clean)
+  (setq show-trailing-whitespace t)
+
+  (auto-fill-mode t)
+  (flyspell-mode t)
+  )
+
 (when (locate-library "markdown-mode")
   (autoload 'markdown-mode "markdown-mode"
     "Major mode for editing Markdown files" t)
 
-  (delete-trailing-whitespace-mode 'clean)
-  (setq show-trailing-whitespace t)
 
   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
   (autoload 'gfm-mode "markdown-mode"
     "Major mode for editing GitHub Flavored Markdown files" t)
-  (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode)))
+  (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+  (add-hook 'markdown-mode-hook #'avm-markdown-mode-hook))
