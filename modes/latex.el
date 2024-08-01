4b72a01602436c3387df353402fbd86f5812c83a
Alexey Martynov <martynov.alexey@gmail.com>
2024-08-01 12:45:24 +0300

    Add Imenu support for LaTeX reference insertion
    The labels are categorized as chapters, sections, figures, tables,
    listings, line numbers in listings and all other according to
    prefixes.
    
    The Imenu version invoked automatically when Emacs running with
    GUI. Without GUI the minibuffer prompt is used as before.

--- modes/latex.el
+++ modes/latex.el
@@ -5,7 +5,9 @@
 (put 'latex-aux-file 'safe-local-variable 'stringp)
 
 (defun latex-list-aux-labels ()
-  "Return list of labels available from current buffer's auxiliary file."
+  "Return list of labels available from current buffer's auxiliary file.
+
+The returned list is list of CONS cells where CAR is a label and CDR is a caption."
   (when (and latex-aux-file
              (file-readable-p latex-aux-file))
     (let ((source latex-aux-file))
@@ -13,26 +15,132 @@
         (insert-file-contents source)
         (let (labels)
           (goto-char (point-min))
-          (while (re-search-forward "\\\\newlabel{\\([^}]*\\)}" nil t)
-            (push (match-string 1) labels))
+          (while (re-search-forward "\\\\newlabel{\\([^}]*\\)}{{[^}]*}{[^}]*}{\\([^}]*\\)}{[^}]*}{[^}]*}}" nil t)
+            (push (cons (match-string 1) (match-string 2)) labels))
           labels)))))
 
 (defun latex-list-buffer-labels (labels)
-  "Return lis of labels available from current buffer appended to LABELS"
+  "Return list of labels available from current buffer appended to LABELS."
   (save-excursion
     (goto-char (point-min))
     (while (re-search-forward "\\\\label{\\([^}]*\\)}" nil t)
       (cl-pushnew (match-string 1) labels))
     labels))
 
+(defun latex-label-info> (a b)
+  "Compare latex label info"
+  (let ((s1 (if (stringp a) a (car a)))
+        (s2 (if (stringp b) b (car b))))
+    (string> s1 s2)))
+
+(defun latex-imenu-create-label-index (document-labels)
+  "Generate Imenu content from DOCUMENT-LABELS.
+
+All labels are split to category submenus according to to prefix: 'chap',
+'sec', 'lst', 'fig' and 'tbl'. Uncategorized labels are placed to 'Other'
+submenu. The listings has additional level: if label has 2 prefixes a submenu
+of listing is created and label placed there."
+  (let ((labels (sort document-labels #'latex-label-info>))
+        chapters
+        sections
+        listings
+        figures
+        tables
+        other
+        result
+        current-listing-id
+        current-listing-caption
+        current-listing-lines
+        last-label)
+    (dolist (item labels)
+      (let* ((label (if (stringp item) item (car item)))
+             (parts (split-string label ":" t "\s"))
+             caption
+             (type (car parts)))
+        (unless (string= last-label label)
+          (setf last-label label)
+          (if (stringp item)
+              ;; Label without caption
+              (setf caption (if (cdr parts) (cadr parts) label))
+            ;; Label with caption
+            (setf caption (cdr item)))
+          (cond
+           ((string= "chap" type)
+            (push (cons (format "%s (%s)" caption (cadr parts)) label) chapters))
+           ((string= "sec" type)
+            (push (cons (format "%s (%s)" caption (cadr parts)) label) sections))
+           ((string= "lst" type)
+            (if (cddr parts)
+                ;; Has line labels
+                (let ((id (cadr parts)))
+                  (if (string= current-listing-id id)
+                      ;; Continue line collecting
+                      (push (cons (caddr parts) label) current-listing-lines)
+                    (progn
+                      ;; New listing id
+                      (when current-listing-lines
+                        (push (cons current-listing-id current-listing-lines) listings))
+                      (setf current-listing-id id
+                            current-listing-caption (format "%s (%s)" caption id)
+                            current-listing-lines (list (cons (caddr parts) label))))))
+              ;; No line numbers
+              (when (string= current-listing-id (cadr parts))
+                ;; Current listing name matches collected line numbers,
+                ;; add special reference
+                (push (cons "Listing" label) current-listing-lines))
+              (when current-listing-lines
+                (push (cons current-listing-caption current-listing-lines) listings))
+              (unless (string= current-listing-id (cadr parts))
+                (push (cons (format "%s (%s)" caption (cadr parts)) label) listings))
+              (setf current-listing-id nil
+                    current-listing-caption nil
+                    current-listing-lines nil)))
+           ((string= "fig" type)
+            (push (cons (format "%s (%s)" caption (cadr parts)) label) figures))
+           ((string= "tbl" type)
+            (push (cons (format "%s (%s)" caption (cadr parts)) label) tables))
+           (t
+            (push (cons (format "%s (%s)" caption label) label) other))))))
+    (when current-listing-lines
+      ;; Pick up uncompleted listing line list
+      (push (cons (or current-listing-caption current-listing-id) current-listing-lines) listings))
+    (when other
+      (push (cons "Other" other) result))
+    (when tables
+      (push (cons "Tables" tables) result))
+    (when figures
+      (push (cons "Figures" figures) result))
+    (when listings
+      (push (cons "Listings" listings) result))
+    (when sections
+      (push (cons "Sections" sections) result))
+    (when chapters
+      (push (cons "Chapters" chapters) result))
+    result))
+
+(defun avm-latex-imenu-label-helper (command)
+  (let (index-alist
+        (result t))
+    ;; Create a list for this buffer only when needed.
+    (while (eq result t)
+      (setf index-alist (latex-imenu-create-label-index (latex-list-buffer-labels (latex-list-aux-labels))))
+      (setf result (imenu--mouse-menu index-alist t))
+      (if (equal result imenu--rescan-item)
+          (progn
+            (imenu--cleanup)
+            (setq result t
+                  imenu--index-alist nil))
+        (insert "\\" command "{" (cdr result) "}"))
+    result)))
+
 (defun latex-insert-reference-impl (command)
   "Insert reference via COMMAND.
 
-If there is a highlighted region, the refernce COMMAND is wrapped
+If there is a highlighted region, the reference COMMAND is wrapped
 around the region text. If LATEX-AUX-FILE is set the list of
 available labels is parsed from file and used as completion in
-interactive prompt. If there is no auxiliary file and now highlighted
-region the COMMAND with empty argument is inserted."
+interactive prompt or Imenu. If there is no auxiliary file and no
+highlighted region the COMMAND with empty argument is inserted."
   (cond
    ((use-region-p)
     (save-excursion
@@ -42,8 +150,12 @@ region the COMMAND with empty argument is inserted."
         (insert "}")
         (goto-char begin)
         (insert "\\" command "{"))))
-    (latex-aux-file
-     (let ((label (completing-read "Label: " (latex-list-buffer-labels (latex-list-aux-labels)))))
+   ((and latex-aux-file window-system)
+    (imenu (avm-latex-imenu-label-helper command)))
+   (latex-aux-file
+    (let ((label (completing-read "Label: " (mapcar #'(lambda (item)
+                                                        (if (stringp item) item (car item)))
+                                                    (latex-list-buffer-labels (latex-list-aux-labels))))))
        (insert "\\" command "{" label "}")))
     (t
      (insert "\\" command "{}")
